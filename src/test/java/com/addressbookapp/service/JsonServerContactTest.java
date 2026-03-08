0f6c5397c520c40fa9949e265f0b8c55864c58a1
Sanya Modi <modisanya05@gmail.com>
2026-03-09 04:43:34 +0530

    [Sanya] : Added UC23 feature to read multiple entries from json server

--- src/test/java/com/addressbookapp/service/JsonServerContactTest.java
+++ src/test/java/com/addressbookapp/service/JsonServerContactTest.java
@@ -1,6 +1,7 @@
 package com.addressbookapp.service;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
@@ -34,9 +35,11 @@ public class JsonServerContactTest {
     @Test
     void shouldReadContactsFromJsonServer() {
 
-        Response response = given()
+        Response response = (Response) given()
                 .when()
-                .get("/contacts");
+                .get("/contacts")
+                .then()
+                .statusCode(200);
 
         assertEquals(200, response.getStatusCode());
 
@@ -51,5 +54,56 @@ public class JsonServerContactTest {
         addressBookMemory.forEach(System.out::println);
 
         assertTrue(addressBookMemory.size() > 0);
+    }
+    
+    /**
+     * UC23
+     * Add multiple contacts to JSON Server
+     */
+    @Test
+    void shouldAddMultipleContactsToJsonServer() {
+
+        Contact contact1 = new Contact(
+                "Thread1",
+                "Test",
+                "Minal",
+                "Bhopal",
+                "MP",
+                "462001",
+                "9999999991",
+                "thread1@gmail.com"
+        );
+
+        Contact contact2 = new Contact(
+                "Thread2",
+                "Test",
+                "Palasia",
+                "Indore",
+                "MP",
+                "452001",
+                "9999999992",
+                "thread2@gmail.com"
+        );
+
+        Contact[] contacts = { contact1, contact2 };
+
+        for(Contact contact : contacts) {
+
+            Response response = given()
+                    .contentType(ContentType.JSON)
+                    .body(contact)
+                    .when()
+                    .post("/contacts");
+
+            assertEquals(201, response.getStatusCode());
+
+            Contact savedContact = response.getBody().as(Contact.class);
+
+            addressBookMemory.add(savedContact);
+
+            System.out.println("Contact added to JSON Server: " + savedContact.getFirstName());
+        }
+
+        assertTrue(addressBookMemory.size() >= 2);
     }
 }
